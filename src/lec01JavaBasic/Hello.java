502771d99bade7959af932bbbe52267a2060f63c
TofaelKabir <tofaelkabir@gmail.com>
2023-12-09 10:20:26 -0500

    Java Basic 01

---
+++ src/lec01JavaBasic/Hello.java
@@ -0,0 +1,8 @@
+package lec01JavaBasic;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Let's learn Java with Fun!");
+	}
+
+}
---
+++ src/lec01JavaBasic/HelloWorld.java
@@ -0,0 +1,38 @@
+package lec01JavaBasic;
+
+// we can create more than one package inside the src file [src is called source file]
+// we can create more than one class inside a package
+
+// Here public --> access modifier, class --> type, HelloWorld --> is the name of the class
+// The system created a class for us, we didn't manually type it
+
+public class HelloWorld {
+	// type 'main', then press Enter to get help from content assist to find main method
+	// main method
+	// it is the entry point of the Java
+	// This is used to execute the code
+	// If it is absent, no execution of code
+	// This is a system generated method, name is 'main'
+	// What is the shortcut of creating a main method? Ans: Type 'main' and then press Enter button.
+	// All the execution come from main method
+	// main method is used 'only once' inside a class [try to create a main method inside/outside the class]
+	public static void main(String[] args) {
+		// type 'syso', then press Enter to get help from content assist to print something
+		// below line is also system generated printing command, used for printing something
+		System.out.println("Hello World!");
+		// common mistake: we forgot to put double quotation inside parenthesis
+		// For execution: We have to save the code (Manually by clicking save/save all button on the top left)
+		// Windows user: keyboard shortcut: save (ctrl+s), saveAll (ctrl+shift+s)
+		// Mac user: keyboard shortcut: save (command+s), saveAll (command+shift+s)		
+	}
+	
+}
+
+// This part is called Editor, where you write the code
+// Below part where you get the outcome as "Hello World!" is called Console
+
+// How to Execute/Run the code? 
+// 1) Green play button on the top
+// 2) right click on Editor -- then 'run as' -- 'java application' 
+// 3) from the top of the page from 'Run' -- then 'run as' -- 'java application' 
+// 4) go to the class on left and do the right click -- then 'run as' -- 'java application' 
