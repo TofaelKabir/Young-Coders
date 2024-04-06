867b0e8c5b378df0ccf08332e54a7ba82aed626f
TofaelKabir <tofaelkabir@gmail.com>
2024-04-06 15:16:02 -0400

    Array

--- src/lec17_coding_challenge/D01_smallestNumber.java
+++ src/lec17_coding_challenge/D01_smallestNumber.java
@@ -1,18 +1,18 @@
 package lec17_coding_challenge;
 
-public class D01_largestNumber {
+public class D01_smallestNumber {
 
 	public static void main(String[] args) {
 		int [] number = {2, 12, 10, 1, 50};
 		
-		int largest = number[0];
+		int smallest = number[0];
 		
 		for(int i = 1; i< number.length; i++) {
-			if(number[i]>largest) {
-				largest = number[i];
+			if(number[i]<smallest) {
+				smallest = number[i];
 			}
 		}
-		System.out.println(largest);
+		System.out.println(smallest);
 
 	}
 
