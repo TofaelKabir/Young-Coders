44ed9eb93b3e37c6179514c31115d7c2ea00b3b1
TofaelKabir <tofaelkabir@gmail.com>
2024-06-08 14:45:59 -0400

    Map

--- src/lec23_02_java_map/Map_01_use_of_hashMap.java
+++ src/lec23_02_java_map/Map_01_use_of_hashMap.java
@@ -1,8 +1,11 @@
 package lec23_02_java_map;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Map_01_use_of_hashMap {
 
@@ -62,6 +65,78 @@ public class Map_01_use_of_hashMap {
 		nameAndAge.put(null, 24);
 		
 		System.out.println(nameAndAge);
+		
+		System.out.println("\n---------------------");
+		System.out.println("Size: " + nameAndAge.size());
+		
+		System.out.println("\n---------------------");
+		// get(K) --> Returns the value to which the specified key is mapped,or null if this map contains no mapping for the key. 
+		System.out.println(nameAndAge.get("Luis"));
+		
+		System.out.println("\n---------------------");
+		// keySet() --> Returns a Set view of the keys contained in this map.
+		Set<String> allKeys = nameAndAge.keySet();
+		System.out.println(allKeys);
+		
+		System.out.println("\n---------------------");
+		Collection<Integer> allValues = nameAndAge.values();
+		System.out.println(allValues);
+		
+		
+		System.out.println("\n---------------------");
+		// We can use for each loop to iterate through the keys.
+		// iteration through Map is not possible by any loop or Iterator.
+		// So, how can we get the value one by one
+		
+		// key is the name of the loop which sorted the key value from allKeys
+		for (String key: allKeys) {
+			System.out.println(key + "=" + nameAndAge.get(key));
+		}
+		
+		System.out.println("\n---------- entrySet() -----------");
+		Set<Map.Entry<String, Integer>> entries = nameAndAge.entrySet(); // tough, don't try to memorize, try to understand
+		System.out.println(entries);
+		
+		System.out.println("\n---------- Iterate -----------");
+		// Iterate
+		for(Map.Entry<String, Integer> entryOneByOne : entries) {
+			System.out.println(entryOneByOne);
+		}
+		
+		// Another example of Map [Same]
+		
+		System.out.println("\n---------------------");
+		Map<String, String> states =new HashMap<>();
+		states.put("Alex", "TX");
+		states.put("Alex", "NY");
+		states.put("Albert", null);
+		states.put("Tiffany", "FL");
+		states.put("Mark", "GA");
+		states.put("Chelsey", null);
+		states.put("John", "NC");
+		states.put(null, "CA");
+		states.put(null, "NM");
+		
+		System.out.println(states);
+		
+		System.out.println(states.get("Joe"));
+		
+		System.out.println("\n---------------------");
+		// keySet() --> Returns a Set view of the keys contained in this map.
+		Set<String> statesKeys = states.keySet();
+		System.out.println(statesKeys);
+		
+		
+		System.out.println("\n---------------------");
+		// We can use for each loop to iterate through the keys.
+		// iteration through Map is not possible by loop or Iterator.
+		// So, how can we get the value one by one
+		
+		// key is the name of the loop which sorted the key value from allKeys
+		for (String stateName: statesKeys) {
+			System.out.println(stateName + "=" + nameAndAge.get(stateName));
+		}
+		
 	}
 
 }
